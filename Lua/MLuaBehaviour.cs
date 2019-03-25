1154b7a68a70c495f42382808947df8251064cc4
GuiLing <2331828200@qq.com>
2019-03-25 08:54:37 +0800

    MLuaBehaviour.cs

---
+++ Lua/MLuaBehaviour.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class MLuaBehaviour : MonoBehaviour
+{
+    public SGTextAsset luaScript;
+
+    internal static LuaEnv luaEnv = new LuaEnv(); //all lua behaviour shared one luaenv only!
+    internal static float lastGCTime = 0;
+    internal const float GCInterval = 1;//1 second 
+
+    private Action luaStart;
+    private Action luaUpdate;
+    private Action luaOnDestroy;
+
+    private LuaTable scriptEnv;
+
+    void Awake()
+    {
+        scriptEnv = luaEnv.NewTable();
+
+        // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
+        LuaTable meta = luaEnv.NewTable();
+        meta.Set("__index", luaEnv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();
+
+        scriptEnv.Set("self", this);
+
+        luaEnv.DoString(luaScript.Text, "MLuaBehaviour", scriptEnv);
+
+        Action luaAwake = scriptEnv.Get<Action>("Awake");
+        scriptEnv.Get("Start", out luaStart);
+        scriptEnv.Get("Update", out luaUpdate);
+        scriptEnv.Get("Ondestroy", out luaOnDestroy);
+
+        if (luaAwake != null)
+        {
+            luaAwake();
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (luaStart != null)
+        {
+            luaStart();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (luaUpdate != null)
+        {
+            luaUpdate();
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (luaOnDestroy != null)
+        {
+            luaOnDestroy();
+        }
+        luaOnDestroy = null;
+        luaUpdate = null;
+        luaStart = null;
+        scriptEnv.Dispose();
+    }
+}
