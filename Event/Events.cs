3c41ea561244a2920cfe548b017dcf5e071f9a90
GuiLing <2331828200>
2019-06-25 23:40:46 +0800

    Event

---
+++ Event/Events.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsStartIndex
+{
+    public const int WSQ = 0;
+    public const int HCQ = 3000;
+}
+
+public enum UIEvent
+{
+    A = EventsStartIndex.WSQ,
+    B,
+    C
+}
+
+public enum XXEvent
+{
+    A = EventsStartIndex.HCQ,
+    B,
+    C
+}
---
+++ Event/Test_Event.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Event : MonoBehaviour
+{
+    void Start()
+    {
+        QEventSystem.RegisterEvent(UIEvent.A, Func0);
+        QEventSystem.UnRegisterEvent(XXEvent.A, Func0);
+        QEventSystem.SendEvent(UIEvent.A);
+    }
+
+    void Func0(int n, params object[] param)
+    {
+        print(n);
+    }
+}
